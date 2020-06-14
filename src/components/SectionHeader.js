dd276899ddbf37efc8cf4b139c62083497e3634d
Amarpreet Singh <46095869+amarmanhala@users.noreply.github.com>
2020-06-14 18:30:26 -0400

    Update SectionHeader.js

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -5,7 +5,7 @@ import H2 from "./designSystem/H2"
 const SectionHeader = props => {
   return (
     <SectionHeaderWrapper>
-      <H2 backgroundColor={true} border={true}>{props.title}</H2>
+      <H2 backgroundColor={true} border={true} style={{ padding: "0px 6px" }}>{props.title}</H2>
     </SectionHeaderWrapper>
   )
 }
