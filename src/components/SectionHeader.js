f4f16abe8fac10c7917772e0f84a6877d36b9f1f
Amarpreet Singh <46095869+amarmanhala@users.noreply.github.com>
2020-06-14 17:56:38 -0400

    Add background.

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -5,7 +5,7 @@ import H2 from "./designSystem/H2"
 const SectionHeader = props => {
   return (
     <SectionHeaderWrapper>
-      <H2>{props.title}</H2>
+      <H2 style={{ backgroundColor: "rgb(238, 238, 238)", border: "2px solid #000" }}>{props.title}</H2>
     </SectionHeaderWrapper>
   )
 }
